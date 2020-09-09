343ced964821fb24036be558d918294c496d1411
Alexander Førsund <alexanderforsund@gmail.com>
2020-09-09 22:15:18 +0200

    demo ready for deployment

--- server.js
+++ server.js
@@ -43,8 +43,8 @@ app.use('/api/auth', authRoute);
 app.use('/api/user', userRoute);
 app.use('/api/game', gameRoute);
 
-app.get('*', (req, res) =>
-	res.sendFile(path.join(__dirname, 'public', 'index.html'))
+app.get('/.*/', (req, res) =>
+	res.sendFile(__dirname + '/public/'));
 );
 
 const PORT = process.env.PORT || 5000;
