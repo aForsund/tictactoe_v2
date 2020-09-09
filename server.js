2afd78372ba8737dd83dff449cae2d91a8aabcee
Alexander Førsund <alexanderforsund@gmail.com>
2020-09-09 21:45:32 +0200

    demo ready for deployment

--- server.js
+++ server.js
@@ -47,4 +47,6 @@ app.get('*', (req, res) =>
 	res.sendFile(path.join(__dirname, 'public', 'index.html'))
 );
 
-app.listen(5000, () => console.log(`Server started on port 5000`));
+const PORT = process.env.PORT || 5000;
+
+app.listen(5000, () => console.log(`Server started on port ${PORT}`));
