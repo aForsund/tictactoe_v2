e7e9527f4b0fc62ed57360107cfe3383e0d3d32f
Alexander Førsund <alexanderforsund@gmail.com>
2020-09-09 22:29:07 +0200

    demo ready for deployment

--- server.js
+++ server.js
@@ -47,6 +47,6 @@ app.use('/api/game', gameRoute);
 
 app.get('/.*/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
 
-const PORT = process.env.PORT || 5000;
+let port = process.env.PORT || 5000;
 
-app.listen(5000, () => console.log(`Server started on port ${PORT}`));
+app.listen(port, () => console.log(`Server started on port ${PORT}`));
