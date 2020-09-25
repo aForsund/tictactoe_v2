fbbaf514c8d1c468611a5d09401b02e30096a638
Alexander Førsund <alexanderforsund@gmail.com>
2020-09-25 11:57:18 +0200

    added client to master branch

---
+++ Client/src/components/PlayerCard.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="card notification is-info">
+    <div class="card-content">
+      <h1 class="title">Player Card</h1>
+      <p class="is-size-5">
+        Name:
+        <span>{{ player.username ? player.username : player }}</span>
+      </p>
+
+      <p class="is-size-5">
+        Games played:
+        <span>{{ player.gamesPlayed ? player.gamesPlayed : 'N/A' }}</span>
+      </p>
+      <p class="is-size-5">
+        ELO:
+        <span>{{ player.rating ? player.rating : 'N/A' }}</span>
+      </p>
+      <footer class="card-footer">
+        <button class="button is-link mt-2">Challenge</button>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    player: [Object, String, Array]
+  }
+};
+</script>
+
+<style scoped>
+.button {
+  transition: all 0.4s;
+}
+.card {
+  margin-top: 0.75rem;
+}
+</style>
---
+++ Client/src/views/Rankings.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="hero-body">
+    <div class="container">
+      <h1 class="title">Rankings</h1>
+
+      <Search @search-string="search" />
+      <PlayerCard v-for="player in playerList" :key="player._id" :player="player" />
+    </div>
+  </div>
+</template>
+
+<script>
+import PlayerCard from "@/components/PlayerCard.vue";
+import Search from "@/components/Search.vue";
+import API_interface from "@/services/API-interface.js";
+
+export default {
+  components: {
+    PlayerCard,
+    Search
+  },
+  computed: {
+    searchArray() {
+      return this.$store.state.searchArray;
+    }
+  },
+  data() {
+    return {
+      playerList: []
+    };
+  },
+  created() {
+    if (this.searchArray.length === 0 || this.searchArray === undefined) {
+      API_interface.getUsers()
+        .then(response => (this.playerList = response.data))
+        .catch(err => console.log(err));
+    } else {
+      this.playerList = this.searchArray;
+    }
+  },
+  methods: {
+    async search(string) {
+      //this.playerList = string;
+
+      console.log("searching for: ", string);
+      console.log("store data: ", this.searchArray);
+      if (string === "") {
+        API_interface.getUsers()
+          .then(response => {
+            this.playerList = response.data;
+            this.updateSearchArray();
+          })
+          .catch(err => console.log(err));
+      } else {
+        API_interface.search(string)
+          .then(response => {
+            this.playerList = response.data;
+            this.updateSearchArray();
+          })
+          .catch(err => console.log(err));
+      }
+    },
+    updateSearchArray() {
+      console.log("trying to update searchArray store with: ", this.playerList);
+      this.$store.commit("rememberSearchResults", this.playerList);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.button {
+  transition: all 0.4s;
+}
+</style>
